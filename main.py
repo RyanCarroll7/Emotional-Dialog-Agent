39d563b860c9cfe7cb142fd0d924caad7cd91327
RyanCarroll7 <ryan.carroll421@gmail.com>
2025-04-09 17:27:03 -0400

    main does random tests of input

--- main.py
+++ main.py
@@ -1,47 +1,31 @@
+from random import random
 from input_classifier import classify_input
 
 
 if __name__ == "__main__":
     # Test classification of same message with different expressions read
-    response_a = classify_input(
-        {
-            "Anger": 0.0,
-            "Contempt": 0.0,
-            "Disgust": 0.0,
-            "Fear": 0.0,
-            "Happiness": 0.0,
-            "Sadness": 1.0,
-            "Surprise": 0.0,
-        },
-        "I'm not sure what to do right now.",
-        "",
-    )
-    response_b = classify_input(
-        {
-            "Anger": 1.0,
-            "Contempt": 0.0,
-            "Disgust": 0.0,
-            "Fear": 0.0,
-            "Happiness": 0.0,
-            "Sadness": 0.0,
-            "Surprise": 0.0,
-        },
-        "I'm not sure what to do right now.",
-        "",
-    )
-    response_c = classify_input(
-        {
-            "Anger": 0.0,
-            "Contempt": 0.0,
-            "Disgust": 0.1,
-            "Fear": 0.1,
-            "Happiness": 0.4,
-            "Sadness": 0.0,
-            "Surprise": 0.4,
-        },
-        "I'm not sure what to do right now.",
-        "",
-    )
-    print("Response A:", response_a)
-    print("Response B:", response_b)
-    print("Response C:", response_c)
+    for i in range(5):
+        values = [random() for _ in range(7)]
+        total = sum(values)
+        values = [round(val / total, 2) for val in values]
+        expression_data = dict(
+            zip(
+                [
+                    "Anger",
+                    "Contempt",
+                    "Disgust",
+                    "Fear",
+                    "Happiness",
+                    "Sadness",
+                    "Surprise",
+                ],
+                values,
+            )
+        )
+        response = classify_input(
+            expression_data,
+            "I'm not sure what to do right now.",
+            "",
+        )
+        print(f"Expression Data: {expression_data}")
+        print(f"Response {i + 1}:", response, "\n")
