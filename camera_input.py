39a085207c1c6923de2d77dfe4f9fb85c7e83ea6
Ryan Carroll <ryan.carroll421@gmail.com>
2025-04-09 20:50:30 -0400

    Can now have deepface capture emotion at button press

--- camera_input.py
+++ camera_input.py
@@ -1,3 +1,20 @@
 from deepface import DeepFace
+import cv2
+import numpy as np
 
-DeepFace.stream("./database")
+
+def analyze_facial_emotion(cap: cv2.VideoCapture, filename: str) -> dict[str, np.float32]:
+    # If the camera is not opened, raise an error
+    if not cap.isOpened():
+        raise IOError("Cannot open camera")
+    # Capture the image (twice ensures we get an unbuffered frame)
+    captured, image = cap.read()
+    captured, image = cap.read()
+    # Check if image is captured correctly
+    if not captured:
+        raise IOError("Cannot capture image")
+    # Save the captured image to the specified filename
+    cv2.imwrite(filename, image)
+    # Analyze the image using DeepFace
+    analysis = DeepFace.analyze(filename, ("emotion"), align=False)
+    return analysis[0]["emotion"]
--- main.py
+++ main.py
@@ -1,31 +1,22 @@
 from random import random
+
+import cv2
+from camera_input import analyze_facial_emotion
 from input_classifier import classify_input
 
 
 if __name__ == "__main__":
-    # Test classification of same message with different expressions read
-    for i in range(5):
-        values = [random() for _ in range(7)]
-        total = sum(values)
-        values = [round(val / total, 2) for val in values]
-        expression_data = dict(
-            zip(
-                [
-                    "Anger",
-                    "Contempt",
-                    "Disgust",
-                    "Fear",
-                    "Happiness",
-                    "Sadness",
-                    "Surprise",
-                ],
-                values,
-            )
-        )
-        response = classify_input(
-            expression_data,
-            "I'm not sure what to do right now.",
-            "",
-        )
-        print(f"Expression Data: {expression_data}")
-        print(f"Response {i + 1}:", response, "\n")
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    if not cap.isOpened():
+        cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        raise IOError("Cannot open camera")
+    while input("Press Enter to capture an image (\"n\" to quit)").strip().lower() != "n":
+        try: 
+            analysis = analyze_facial_emotion(cap, "captured_image.jpg")
+        except Exception as e:
+            print(f"Error analyzing facial emotion: {e}")
+            continue
+        print("Captured image and analyzed emotions:")
+        print('\t' + '\n\t'.join([f"{k}: {v:.2f}" for k, v in analysis.items()]))
+
